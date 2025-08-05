1922116e0ea46db577406708e38c8bb75253e27d
MrakKor <mkormarkor@gmail.com>
2025-08-05 23:58:36 +0300

    Update fur_streamlit.py

--- fur_streamlit.py
+++ fur_streamlit.py
@@ -10,6 +10,48 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 
+#ТАБЛИЦЫ
+SERVICE_ACCOUNT_INFO = st.secrets["gcp_service_account"]
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(SERVICE_ACCOUNT_INFO, scope)
+client = gspread.authorize(credentials)
+spreadsheet = client.open("mogo")
+
+json_urls = [
+    "https://raw.githubusercontent.com/MrakKor/MOGO/main/history_blau_old.json",
+    "https://raw.githubusercontent.com/MrakKor/MOGO/main/history_oben_old.json",
+    "https://raw.githubusercontent.com/MrakKor/MOGO/main/lager_blau_old.json",
+    "https://raw.githubusercontent.com/MrakKor/MOGO/main/lager_oben_old.json"
+]
+sheet_names = ["history_blau", "history_oben", "lager_blau", "lager_oben"]
+
+for url, sheet_name in zip(json_urls, sheet_names):
+    try:
+        worksheet = spreadsheet.worksheet(sheet_name)
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = spreadsheet.add_worksheet(title=sheet_name, rows="1000", cols="50")
+
+    response = requests.get(url)
+    try:
+        data = response.json()
+    except JSONDecodeError:
+        st.error(f"Fehler beim Parsen von JSON von {url}")
+        continue
+
+    if isinstance(data, list) and len(data) > 0:
+        worksheet.batch_clear(["A1:Z1000"])
+        header = [str(k) if k is not None else "" for k in data[0].keys()]
+        worksheet.append_row(header)
+        if daten is not None:
+            rows = [[key, value] for key, value in daten.items()]
+        else:
+            rows = []
+        worksheet.update("A2", rows)
+
 st.image("logo.png", width=200)
 
 #Пароль
@@ -439,45 +481,3 @@ elif menu.startswith("4"):
                     edited_lager["__zeit"] = valid_zeit
                     set_lager(hotel, edited_lager, manuelle_datum=True)  
                     st.success("✅ Lager wurde gespeichert")
-
-#ТАБЛИЦЫ
-SERVICE_ACCOUNT_INFO = st.secrets["gcp_service_account"]
-
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(SERVICE_ACCOUNT_INFO, scope)
-client = gspread.authorize(credentials)
-spreadsheet = client.open("mogo")
-
-json_urls = [
-    "https://raw.githubusercontent.com/MrakKor/MOGO/main/history_blau_old.json",
-    "https://raw.githubusercontent.com/MrakKor/MOGO/main/history_oben_old.json",
-    "https://raw.githubusercontent.com/MrakKor/MOGO/main/lager_blau_old.json",
-    "https://raw.githubusercontent.com/MrakKor/MOGO/main/lager_oben_old.json"
-]
-sheet_names = ["history_blau", "history_oben", "lager_blau", "lager_oben"]
-
-for url, sheet_name in zip(json_urls, sheet_names):
-    try:
-        worksheet = spreadsheet.worksheet(sheet_name)
-    except gspread.exceptions.WorksheetNotFound:
-        worksheet = spreadsheet.add_worksheet(title=sheet_name, rows="1000", cols="50")
-
-    response = requests.get(url)
-    try:
-        data = response.json()
-    except JSONDecodeError:
-        st.error(f"Fehler beim Parsen von JSON von {url}")
-        continue
-
-    if isinstance(data, list) and len(data) > 0:
-        worksheet.batch_clear(["A1:Z1000"])
-        header = [str(k) if k is not None else "" for k in data[0].keys()]
-        worksheet.append_row(header)
-        if daten is not None:
-            rows = [[key, value] for key, value in daten.items()]
-        else:
-            rows = []
-        worksheet.update("A2", rows)
